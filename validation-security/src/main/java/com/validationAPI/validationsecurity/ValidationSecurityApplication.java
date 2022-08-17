288c32acea93bc30a1b5b54790d4feee8963bcdc
Rafael Enes <rafaeleneswork@gmail.com>
2022-08-17 07:13:25 -0300

    first commit

---
+++ validation-security/src/main/java/com/validationAPI/validationsecurity/ValidationSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.validationAPI.validationsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidationSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidationSecurityApplication.class, args);
+	}
+
+}
